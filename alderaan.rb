6d8ba5bcab176d61b00fcf1bfcb55cf8fd61478d
Valeria Benetti <vbenetti46@gmail.com>
2017-04-20 17:01:42 -0400

    Main option menu is up but not running

--- alderaan.rb
+++ alderaan.rb
@@ -1,2 +1,7 @@
 # require 'ap'
 # require_relative 'film'
+
+loop do
+  puts "Which Star Wars films would you like to know about? \n\ Episode (1): The Phantom Menace \n\ Episode (2): Attack of the Clones \n\ Episode (3): Revenge of the Sith \n\ Episode (4): A New Hope \n\ Episode (5): The Empire Strikes Back \n\ Episode (6): Return of the Jedi \n\ Episode (7): The Force Awakens"
+  selected = gets.chomp.upcase
+end
--- film.rb
+++ film.rb
@@ -2,10 +2,6 @@ require 'httparty'
 require 'json'
 require 'ap'
 
-film = HTTParty.get("http://swapi.co/api/films")
+class StarWars
 
-questions = JSON(film.body)
-ap questions
-# p film.code
-# p film.message
-# p film.headers
+end
