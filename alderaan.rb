84d7d57d286721b25d88a459c40d27e0485c1dac
Valeria Benetti <vbenetti46@gmail.com>
2017-04-20 15:27:14 -0400

    I can pull a list of the Star Wars films

--- alderaan.rb
+++ alderaan.rb
@@ -1,2 +1,2 @@
-require 'ap'
-require_relative 'film'
+# require 'ap'
+# require_relative 'film'
--- film.rb
+++ film.rb
@@ -1,2 +1,11 @@
 require 'httparty'
 require 'json'
+require 'ap'
+
+film = HTTParty.get("http://swapi.co/api/films")
+
+questions = JSON(film.body)
+ap questions
+# p film.code
+# p film.message
+# p film.headers
